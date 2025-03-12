32547ba1a298c56085aebb8476b924635d423d7a
phihung1902 <101974415+phihung1902@users.noreply.github.com>
2025-03-12 21:04:36 +0700

    Update isAuthenticated.js

--- server/middlewares/isAuthenticated.js
+++ server/middlewares/isAuthenticated.js
@@ -2,9 +2,11 @@ import jwt from "jsonwebtoken";
 
 const isAuthenticated = async (req, res, next) => {
   try {
-console.log("Cookies:", req.cookies);
+    console.log("Cookies:", req.cookies);
+    console.log("Authorization Header:", req.headers.authorization);
     
-    const token = req.cookies.token;
+    const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
+    console.log("Extracted Token:", token);
     if (!token) {
       return res.status(401).json({
         message: "User not authenticated",
